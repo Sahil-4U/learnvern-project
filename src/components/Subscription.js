de6162cfd4410cb9d56151a39cdf53396eccc478
Sahil <ss6611786@gmail.com>
2023-06-29 16:56:12 +0530

    now from static values i have shifted to dynamic values in our custom component

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Subscription.css';
 
 function Subscription() {
+    let date = new Date('2023', '06', '29');
+    let title = 'Monthly subscription';
+    let amount = '124.90';
     return (
         <div className='subscription'>
-            <div>Date</div>
-            <h2 className='subscription_title'>Title</h2>
-            <div className='subscription_price'>Amount</div>
+            <div>{date.toISOString()}</div>
+            <h2 className='subscription_title'>{title}</h2>
+            <div className='subscription_price'>{amount}</div>
         </div>
     )
 }
