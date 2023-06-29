696eb00e2464c5f6346dfb309d9b10482d4ff40c
Sahil <ss6611786@gmail.com>
2023-06-29 17:34:24 +0530

    date object is further divided into 3 components 1 is month 2 is day 3 is year

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './Subscription.css';
 
 function Subscription({ date, amount, title }) {
-
+    const month = date.toLocaleString('default', { month: 'long' });
+    const day = date.toLocaleString('default', { day: '2-digit' });
+    const year = date.getFullYear();
     return (
         <div className='subscription'>
-            <div>{date.toISOString()}</div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
             <h2 className='subscription_title'>{title}</h2>
             <div className='subscription_price'>{amount}</div>
         </div>
