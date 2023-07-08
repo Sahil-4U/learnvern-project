393522eec281b8eccb1d29a73ef96dd653c3b29d
Sahil <ss6611786@gmail.com>
2023-07-08 09:23:59 +0530

    added filter functionality

--- src/App.js
+++ src/App.js
@@ -6,40 +6,42 @@ import { useState } from 'react';
 
 function App() {
   const [filtered, setFiltered] = useState('2021');
-  let subscriptions = [
-    {
-      id: '1',
-      date: new Date('2023', '06', '29'),
-      title: 'Monthly subscription',
-      amount: '124.90',
-    },
-    {
-      id: '2',
-      date: new Date('2024', '06', '29'),
-      title: 'Yearly subscription',
-      amount: '154.90',
-    },
-    {
-      id: '3',
-      date: new Date('2025', '06', '29'),
-      title: 'Quartly subscription',
-      amount: '924.90',
-    }
-  ]
+  const INITIAL_SUBSCRIPTION = [{
+    id: '1',
+    date: new Date('2023', '06', '29'),
+    title: 'Monthly subscription',
+    amount: '124.90',
+  },
+  {
+    id: '2',
+    date: new Date('2024', '06', '29'),
+    title: 'Yearly subscription',
+    amount: '154.90',
+  },
+  {
+    id: '3',
+    date: new Date('2025', '06', '29'),
+    title: 'Quartly subscription',
+    amount: '924.90',
+  }]
+  const [subscriptions, setSubscriptions] = useState(INITIAL_SUBSCRIPTION);
   const addNewSubscription = (data) => {
-    subscriptions.push(data);
-    console.log("app.js", data, subscriptions);
+    setSubscriptions(prev => [data, ...subscriptions]);
+    console.log("app.js", data);
   }
   const filteredData = (data) => {
     setFiltered(data);
   }
+  const filteredArray = subscriptions.filter((data) => {
+    return data.date.getFullYear().toString() === filtered;
+  })
   return (
     <div>
       <NewSubscription addNewSubscription={addNewSubscription} />
       <Filter filteredData={filteredData} valueOfFilter={filtered} />
-      {subscriptions.map((data) => {
+      {filteredArray.map((data) => {
         return (
-          <Subscription date={data.date} title={data.title} amount={data.amount} />
+          <Subscription key={data.id} date={data.date} title={data.title} amount={data.amount} />
         )
       })}
     </div>
